213636bb74c7796371bc9cd2b6fb17b99552ad4d
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-09-15 06:32:20 +0000

    Added few more tables

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+
+@total_revenue = Store.sum('annual_revenue')
+@avg_revenue = Store.average('annual_revenue')
+@rich_stores = Store.where('annual_revenue > 1000000').count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+    has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+    belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Fedor", last_name: "Mashoshin", hourly_rate: 50)
+@store2.employees.create(first_name: "Alice", last_name: "Mashoshina", hourly_rate: 70)
+@store2.employees.create(first_name: "Tima", last_name: "Juma", hourly_rate: 45)
+
